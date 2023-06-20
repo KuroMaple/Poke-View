9c59b311f00b4205a4e05c0e4577969dbeb46f93
Hassan Hashmi <hhashmi@janacorporation.com>
2023-06-20 10:13:05 -0400

    filterbox dropdown functioning as expected

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,56 +6,53 @@ import FrontCard from './FrontCard';
 import { motion } from 'framer-motion';
 
 interface Props {
-  id: number;
+  pkmn: Pokemon;
 }
 
-const Card: React.FC<Props> = ({ id }) => {
+const Card: React.FC<Props> = ({ pkmn }) => {
   // State that holds all the pokemon Data information
   const [pokemon, setPokemon] = useState<Pokemon>(pkmnMaxstats);
   // Determines cards Flip status
   const [isFlipped, setIsFlipped] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const api = pokeAPI;
-        const pokemonData = await api.getPokemon(id); //API calls
-        const speciesData = await api.getSpecies(id);
-        // Process the fetched data
-        const pkmnData: Pokemon = {
-          name: pokemonData.name,
-          id: pokemonData.id,
-          hp: pokemonData.stats[0].base_stat,
-          atk: pokemonData.stats[1].base_stat,
-          def: pokemonData.stats[2].base_stat,
-          front_sprite: pokemonData.sprites.front_default,
-          typePrimary: pokemonData.types[0].type.name,
-        };
+    // const fetchData = async () => {
+    //   const api = pokeAPI;
+    //   const pokemonData = await api.getPokemon(id); //API calls
+    //   const speciesData = await api.getSpecies(id);
+    //   // Process the fetched data
+    //   const pkmnData: Pokemon = {
+    //     name: pokemonData.name,
+    //     id: pokemonData.id,
+    //     hp: pokemonData.stats[0].base_stat,
+    //     atk: pokemonData.stats[1].base_stat,
+    //     def: pokemonData.stats[2].base_stat,
+    //     front_sprite: pokemonData.sprites.front_default,
+    //     typePrimary: pokemonData.types[0].type.name,
+    //   };
 
-        if (pokemonData.types.length === 2) {
-          pkmnData.typeSecondary = pokemonData.types[1].type.name;
-        }
+    //   if (pokemonData.types.length === 2) {
+    //     pkmnData.typeSecondary = pokemonData.types[1].type.name;
+    //   }
 
-        // Parse through flavor text until english flavor is found
-        let counter = -1;
-        let flavorText = '';
-        do {
-          ++counter;
-          flavorText = speciesData.flavor_text_entries[counter].flavor_text;
-        } while (
-          speciesData.flavor_text_entries[counter].language.name !== 'en' //control language from here
-        );
+    //   // Parse through flavor text until english flavor is found
+    //   let counter = -1;
+    //   let flavorText = '';
+    //   do {
+    //     ++counter;
+    //     flavorText = speciesData.flavor_text_entries[counter].flavor_text;
+    //   } while (
+    //     speciesData.flavor_text_entries[counter].language.name !== 'en' //control language from here
+    //   );
 
-        setPokemon((prevState) => ({
-          ...prevState,
-          ...pkmnData,
-          flavorText,
-        }));
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData();
+    //   setPokemon((prevState) => ({
+    //     ...prevState,
+    //     ...pkmnData,
+    //     flavorText,
+    //   }));
+    // };
+    // fetchData();
+    setPokemon(pkmn);
   }, []);
 
   const flipper = () => {
--- src/components/CardDisplay.tsx
+++ src/components/CardDisplay.tsx
@@ -4,6 +4,8 @@ import Card from './Card';
 import { motion } from 'framer-motion';
 import React from 'react';
 import { TypeContext } from '../context/typeContext';
+import { Pokemon } from '../data';
+import pokeAPI from '../poke-api';
 
 const CardDisplay = () => {
   // Contains the selected type to filter by
@@ -17,17 +19,82 @@ const CardDisplay = () => {
 
   // Array corresponding to each unique poke ID
   const [uniquePokeIDs, setUniquePokeIDs] = useState<number[]>([]);
+
+  // State that holds all the pokemon Data information
+  const [pokemonCards, setPokemonCards] = useState<Pokemon[]>([]);
+
+  const generatePokemon = async (id: number): Promise<Pokemon> => {
+    const api = pokeAPI;
+    const pokemonData = await api.getPokemon(id); //API calls
+    const speciesData = await api.getSpecies(id);
+    // Process the fetched data
+    const pkmnData: Pokemon = {
+      name: pokemonData.name,
+      id: pokemonData.id,
+      hp: pokemonData.stats[0].base_stat,
+      atk: pokemonData.stats[1].base_stat,
+      def: pokemonData.stats[2].base_stat,
+      front_sprite: pokemonData.sprites.front_default,
+      typePrimary: pokemonData.types[0].type.name,
+    };
+
+    if (pokemonData.types.length === 2) {
+      pkmnData.typeSecondary = pokemonData.types[1].type.name;
+    }
+    // Parse through flavor text until english flavor is found
+    let counter = -1;
+    let flavorText = '';
+    do {
+      ++counter;
+      flavorText = speciesData.flavor_text_entries[counter].flavor_text;
+    } while (
+      speciesData.flavor_text_entries[counter].language.name !== 'en' //control language from here
+    );
+    pkmnData.flavorText = flavorText;
+    return pkmnData;
+  };
+
+  // Checks the unqiuness of the passed id
+  const isUnique = (id: number): boolean => {
+    pokemonCards.forEach((curPkmn) => {
+      if (curPkmn.id === id) {
+        return false;
+      }
+    });
+
+    return true;
+  };
+
+  // Generates a unique ID, makes the API calls and adds to the Pokemon Array
   const handleAddCards = () => {
     // eslint-disable-next-line no-constant-condition
     let numFound = false;
     let r = 0;
     while (!numFound) {
       r = randomPokemon500();
-      if (uniquePokeIDs.indexOf(r) === -1) {
+      if (isUnique(r)) {
         numFound = true;
       }
     }
-    setUniquePokeIDs((oldArray) => [r, ...oldArray]);
+    generatePokemon(r).then((newPkmn) => {
+      setPokemonCards((oldArray) => [newPkmn, ...oldArray]);
+    });
+    console.log(pokemonCards);
+  };
+
+  // Evaluates whether currentPkmn type matches drop down selected type
+  const typeDisplay = (
+    currentPrimaryType: string,
+    currentSecondaryType: string | undefined
+  ) => {
+    if (providedValue.type === 'all') {
+      return true;
+    } else {
+      return (
+        currentPrimaryType === providedValue.type ||
+        currentSecondaryType === providedValue.type
+      );
+    }
   };
 
   return (
@@ -48,14 +115,16 @@ const CardDisplay = () => {
         initial="initial"
         animate="animate"
       >
-        {/* {uniquePokeIDs
-          .filter((type) => type === providedValue.type)
-          .map((id) => (
-            <Card key={id} id={id} />
-          ))} */}
-        {uniquePokeIDs.map((id) => (
+        {pokemonCards
+          .filter((curPkmn) =>
+            typeDisplay(curPkmn.typePrimary, curPkmn.typeSecondary)
+          )
+          .map((curPkmn) => (
+            <Card key={curPkmn.id} pkmn={curPkmn} />
+          ))}
+        {/* {uniquePokeIDs.map((id) => (
           <Card key={id} id={id} />
-        ))}
+        ))} */}
       </motion.div>
     </div>
   );
