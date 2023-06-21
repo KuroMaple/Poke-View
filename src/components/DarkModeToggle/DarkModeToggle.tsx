338cee3f67a8955b3c66904cae78dd8868a906dd
Hassan Hashmi <hhashmi@janacorporation.com>
2023-06-21 12:06:48 -0400

    dark mode functioning

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 const DarkModeToggle = () => {
@@ -7,13 +7,22 @@ const DarkModeToggle = () => {
   const toggleDarkMode = (checked: boolean) => {
     setDarkMode(checked);
   };
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('darkMode');
+    } else {
+      document.body.classList.remove('darkMode');
+    }
+  }, [isDarkMode]);
+
   return (
     <div className="main-header__button">
       <DarkModeSwitch
         style={{
-          marginBottom: '2rem',
           width: '2rem',
           cursor: 'pointer',
+          height: '2rem',
         }}
         checked={isDarkMode}
         onChange={toggleDarkMode}
